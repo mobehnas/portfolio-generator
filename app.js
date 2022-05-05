2fa853c0d60ef42c33c878c27f8561bb4ee06882
mohamed behnas <behna@DESKTOP-T1K4JBI.(none)>
2022-05-05 11:24:46 -0400

    require

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
-const generatePage = require('./src/page-template');
-
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2);
-
-console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
-console.log(name, github);
 
-const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
